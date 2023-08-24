ba8d53a4a821504ee5ae59c361ad0ef4b04013a8
jgohl-dci <johannes.gohl@dci-student.org>
2023-08-24 10:21:13 +0200

    done

--- solution.py
+++ solution.py
@@ -1,41 +1,41 @@
 # # Task 1
-# i = 0
+i = 0
 
-# while i < 3:
-#     number = int(input('Enter number: '))
-#     i += 1
-#     number = number & 1
+while i < 3:
+    number = int(input('Enter number: '))
+    i += 1
+    number = number & 1
 
-#     if number == 0:
-#         print('Even number')
-#     else:
-#         print('Odd number')
+    if number == 0:
+        print('Even number')
+    else:
+        print('Odd number')
 
 
 
 # # Task 2
-# output = 0
-# i = 0
+output = 0
+i = 0
 
-# while i < 3:
-#     number = int(input('Enter number: '))
-#     i += 1
-#     output += number
+while i < 3:
+    number = int(input('Enter number: '))
+    i += 1
+    output += number
 
-# print('Sum of the numbers: ', output)
+print('Sum of the numbers: ', output)
 
 
 
 # Task 3
-# i = 0
-# output = []
+i = 0
+output = []
 
-# while i < 5:
-#     number = int(input('Enter number: '))
-#     i += 1
-#     output.append(number)
+while i < 5:
+    number = int(input('Enter number: '))
+    i += 1
+    output.append(number)
 
-# print('Highest number: ', max(output))
+print('Highest number: ', max(output))
 
 
 
@@ -45,3 +45,21 @@ number = int(input('Enter number: '))
 for i in range(2, number + 1):
     if number % i == 0:
         print(i)
+
+
+
+# Task 5
+number = int(input('Enter number: '))
+
+if number % 3 == 0 and number % 2 == 0:
+    print(number, 'is even and divisible by 3')
+
+
+
+# Task 6
+number = int(input('Enter number: '))
+
+if number >= 0 and number % 7 == 0 and number % 2 != 0:
+    print(number, 'is positive, odd and divisible by 7')
+else:
+    print('The number is not positive, divisible by 7 or even')
